4a8ddcc81f641ef1559a57c96157a0cf009db6fd
Lidia Sánchez Mérida <lidiasm96@correo.ugr.es>
2019-11-06 19:05:12 +0100

    Nueva versión de la clase y los tests

--- estacion.py
+++ estacion.py
@@ -4,19 +4,19 @@
 Created on Wed Oct 30 13:41:46 2019
 """
 from estadisticas import actualizar_estadisticas
-import numpy as np
+import math
 
 ### Clase que representa los datos de una determinada localización
 class Estacion:
     
     # Crear datos 
     def __init__(self, provincia, municipio, estacion, direccion):
-        self.indicadores = np.array([[]])
+        self.indicadores = []
         self.provincia = provincia
         self.municipio = municipio
         self.estacion = estacion
         self.direccion = direccion
-        self.datos_estadisticos = np.array([[]])
+        self.datos_estadisticos = []
     
     # Asumimos que en el argumento se encuentran solo seis elementos correspondientes
     # a las seis columnas de la web de datos medioambientales.
@@ -25,20 +25,20 @@ class Estacion:
         if nuevos_indicadores.fecha_hora == None:
             return 'Dato no valido'
         else: 
-            np.append(self.indicadores, nuevos_indicadores)
+            self.indicadores.append(nuevos_indicadores)
             return 'Dato valido'
     
     # Los datos estadísticos comparten la misma estructura que los indicadores
     # exceptuando la fecha.
     # Cada fila tiene 5 columnas: la media de so2, part, no2, co, o3.
     def generar_estadisticas(self):
-        nuevas_estadisticas = np.array(actualizar_estadisticas(self.indicadores[:,1], 
+        nuevas_estadisticas = actualizar_estadisticas(self.indicadores[:,1], 
              self.indicadores[:,2], self.indicadores[:,3], self.indicadores[:,4],
-             self.indicadores[:,5]))
-        if (np.isnan(nuevas_estadisticas)):
+             self.indicadores[:,5])
+        if (math.isnan(nuevas_estadisticas)):
             return 'Estadísticas incorrectas'
         else:
-            np.append(self.datos_estadisticos, nuevas_estadisticas)
+            self.datos_estadisticos.append(nuevas_estadisticas)
             return 'Estadísticas correctas'
 
         
--- estadisticas.py
+++ estadisticas.py
@@ -5,8 +5,6 @@ Created on Tue Nov  5 13:41:19 2019
 
 """
 
-import numpy as np
-
 class Estadisticas:
     
     # Crear datos 
@@ -18,11 +16,11 @@ class Estadisticas:
         self.o3_media = o3_media
         
     def actualizar_estadisticas(self, so2_datos, part_datos, no2_datos, co_datos, o3_datos):
-        self.so2_media = np.mean(so2_datos)
-        self.n_medio_part = np.mean(part_datos)
-        self.no2_media = np.mean(no2_datos)
-        self.co_media = np.mean(co_datos)
-        self.o3_media = np.mean(o3_datos)
+        self.so2_media = sum(so2_datos)/len(so2_datos)
+        self.n_medio_part = sum(part_datos)/len(part_datos)
+        self.no2_media = sum(no2_datos)/len(no2_datos)
+        self.co_media = sum(co_datos)/len(co_datos)
+        self.o3_media = sum(o3_datos)/len(o3_datos)
         return [self.so2_media, self.n_medio_part, self.no2_media, self.co_media, self.o3_media]
         
 
